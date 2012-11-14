9ea3f29ba5c261857d56f65f7c538602ec14b074
Alex Hoogkamer <AQHoogkamer@hotmail.com>
2012-11-14 10:56:30 +0100

    bgse opdracht
    het 2de python script en het trim script

---
+++ QC2.py
@@ -0,0 +1,99 @@
+#!/usr/bin/python
+import time
+'''
+Created on 20 sep. 2012
+
+@author: Alex
+'''
+
+if __name__ == '__main__':
+    pass
+
+def QCscript():
+    reads = 0
+    index = 0
+    lengthlist = []
+    GCcontent = []
+    GClist = []
+    listreadcount = [] # a list for counting the number of reads. this is for calculating the GC content per position
+    totalGCcontent = 0
+    totallength = 0
+    maxlength = 0
+    minlength = 0
+    with open("%s.txt" % filename, 'rU') as file1:# open the file
+        for line in file1:
+            if line[0] == '@':
+                reads = reads + 1 # this counts the number of reads
+            else:
+                index = index + 1 # this counter is used to identify the contents of the line
+                    
+            if (index % 3) == 1: # if index divided by 3 leaves 1 than the current line is a line with a DNA sequence
+                line = line.strip("\n")
+                line = line.strip("N")# if the sequence starts with an N it needs to be removed
+                lenread = len(line)
+                lengthlist.append(lenread)# make a list with length of all reads
+                Gcontent = line.count('C')# count the number of C
+                Ccontent = line.count('G')# count the number of G
+                GCcontentread = Gcontent + Ccontent# calculate the GC content of the read
+                totalGCcontent = totalGCcontent + GCcontentread # this is used to store the total GC content of all reads
+                totallength = totallength + lenread # this is used to store the total length of all reads
+                GCcontentread = GCcontentread/(lenread*0.01) # GC content of read
+                GCcontent.append(GCcontentread) # add GC percentage to the list
+                            
+                if lenread > maxlength: # if the read length is larger than maxlength it is the largest read
+                    maxlength = lenread
+                elif minlength == 0:# if the minlength = 0 the current read length will be assigned as the smallest read
+                    minlength = lenread
+                elif lenread < minlength: # if the read length is smaller than minlength it is the smallest read
+                    minlength = lenread # if the minlength = 0 than all raeds are of the same length
+                                
+                positionindex = 0
+                for position in line:
+                    Ccount = position.count("C") # count the G content of the position
+                    Gcount = position.count("G") # count the G content of the position
+                    GCcount = Gcount + Ccount
+                    if index == 1: # if it is the first sequence the values need to be appended to the list
+                        GClist.append(GCcount)
+                        listreadcount.append(1)# when calculating the percentage of G and C the number of reads is neccecary
+                    else: # if it is not the first sequence the values need to be added to the existing values or append to the end of the list
+                        try:
+                            GClist[positionindex] = GClist[positionindex] + GCcount
+                            listreadcount[positionindex] = listreadcount[positionindex] +1
+                        except: # when a read is longer than the list the values will be appended to the list
+                            GClist.append(GCcount)
+                            listreadcount.append(1)
+                    positionindex = positionindex +1
+            else:
+                pass        
+                                
+        
+        mean = totallength / reads # the total length is divided by the total number of reads to calculade the mean length        
+        globalGC = totalGCcontent/(totallength*0.01) # calculate global GC content
+        for position in range(len(GClist)): # add GC content per position to list
+            GClist[position] = GClist[position]/(listreadcount[position]*0.01) # calculate percentage GC per position and add it to list
+        
+    return(reads, mean, minlength, maxlength, GCcontent, globalGC, GClist)
+    file1.close
+
+filename = 's_2_1_sequence-trimmed'
+localtime = time.asctime(time.localtime(time.time()))
+#print("Local current time :", localtime)
+file2 = open("%s results" %filename,'a')
+file2.write("script started at:")
+file2.write("%s\n" % localtime)
+file2.write("this is the results file of %s" %filename)
+results = QCscript()
+file2.write("these are the results of the QC script.\n")
+file2.write("the number of reads is: %s.\n" %results[0])
+file2.write("the longest read is: %s.\n" %results[3])
+file2.write("the shortest read is: %s.\n" %results[2])
+file2.write("the mean of reads is: %s.\n" %results[1])
+file2.write("the GC content of all reads is: %s.\n" %results[5])
+file2.write("the GC content per read is: %s.\n" %results[4])
+file2.write("the GC content per position is: %s.\n" %results[6])
+localtime = time.asctime( time.localtime(time.time()) )
+#print("Local current time :", localtime)
+file2.write("script Finished at:")
+file2.write("%s\n\n" % localtime)
+file2.close
+print('finished')
---
+++ trim.py
@@ -0,0 +1,98 @@
+#!/usr/bin/python
+'''
+Created on 26 sep. 2012
+
+@author: Alex
+'''
+import time
+
+if __name__ == '__main__':
+    pass
+
+def trim(file1):
+    file3 = []
+    readindex = 0
+    with open('%s.txt' %file1, 'rU') as file2:
+        for line in file2:
+            line = line.strip('\n')
+            readindex = readindex + 1
+            file3.append(line)
+            if readindex == 4:
+                readindex = 0
+                seq = file3[1]
+                if seq[0] == 'N':
+                    file3[1] = file3[1].strip('N')
+                    tempstring = file3[3]
+                    file3[3] = tempstring[1:len(file3[3])]
+                else:
+                    pass
+                length1 = len(file3[1])
+                '''
+                a quality score of 13 roughly equals a p-value of 0.05
+                this means that a base with a score 13 or N has a 5% chance of being incorrect
+                any score higher than 13 has less than 5% chance of being incorrect
+                I believe this to be a good cutoff point for quality
+                
+                I have set the trim length to 5 basepares. this means that at least 5 basepares 
+                in a row need to have a score of lower than 13 before I trim that from the read
+                '''
+                file6 = open('scoretable Illumina 1.5.txt','rU')# this loads a scoretable for the specific machine used to make the fastq file
+                file7 = file6.readline()
+                file6.close
+                scoretable = file7.split(' ')
+                badscoreseq = 0
+                positionindex = 0
+                for position in file3[3]:
+                    score = (scoretable.index(position)) + 1
+                    if badscoreseq < 5:
+                        if score < 13:
+                            badscoreseq = badscoreseq + 1
+                        else:
+                            badscoreseq = 0
+                    else:
+                        if score < 13:
+                            badscoreseq = badscoreseq + 1
+                            if positionindex == (length1-1):
+                                seq = file3[3]
+                                seq2 = file3[1]
+                                seq = seq[:(positionindex - badscoreseq + 1)]
+                                seq2 = seq2[:(positionindex - badscoreseq + 1)]
+                                file3[1] = seq2
+                                file3[3] = seq
+                        else:
+                            seq = file3[3]
+                            seq2 = file3[1]
+                            seq = seq[positionindex:]
+                            seq2 = seq2[positionindex:]
+                            file3[1] = seq2
+                            file3[3] = seq
+                            badscoreseq = 0
+                            positionindex = 0
+                    positionindex = positionindex + 1
+                if file3[1] == '':
+                    pass
+                elif len(file3[1]) < (length1*0.25):
+                    pass 
+                else:
+                    with open('%s-trimmed.txt' %file1, 'a') as file4:#open the file
+                        for line in file3:
+                            file4.write('%s\n' %line)
+			file4.close      
+                file3 = []
+            else:
+                pass
+    file2.close
+
+localtime1 = time.asctime(time.localtime(time.time()))
+file1 = ('s_2_1_sequence')
+file3 = open("%s-time" %file1,'a')
+file3.write("script started at:")
+file3.write("%s\n"% localtime1)
+trim(file1)
+file3 = open("%s-results" %file1,'a')
+file3.write("this is the time of the trim script on %s.\n" %file1)
+file3.write("script finished at:")
+localtime2 = time.asctime(time.localtime(time.time()))
+file3.write("%s\n" %localtime2)
+file3.close
+print('finished')
